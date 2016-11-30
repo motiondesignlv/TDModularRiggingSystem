53e39cd7b9df492389c00d38d13e2f8697b951df
Ryuji Tada <ezwed-dewze-v3@ezweb.ne.jp>
2016-11-30 17:08:18 +0900

    Create Spine Rig

--- Spine.py
+++ Spine.py
@@ -8,35 +8,62 @@ class TDSpineRigging():
         #TDMRS.__init__()
         self.TDMRS  = RiggingSystem.ModularRiggingSystem()
         self.spineCtls    = [] #背骨コントローラー
-        self.spineCtlNull = [] #背骨コントローラーのオフセットノード
+        self.spineCtlOffset  = [] #背骨コントローラーのオフセットノード
+        self.spineCtlNull = [] #背骨コントローラーのヌルノード
         self.spineNull    = [] #背骨ジョイントのオフセットノード
 
     "-----コマンドの実行-----"
-    def setSpineRigging(self,parentName,CtlColor):
-        self.SpineCtlList = TDSpineRigging.createFKSpineCtl(self,CtlColor)
-        TDSpineRigging.createFKSpineCtlConnection(self)
+    def setSpineRigging(self,spineJoints,CtlColor):#,parentName,
+        TDSpineRigging.createSpineJoint(self,spineJoints)
+        TDSpineRigging.createSpineFKJoint(self,spineJoints)
+        self.SpineCtlList = TDSpineRigging.createSpineFKCtl(self,spineJoints,CtlColor)
+        TDSpineRigging.createSpineFKCtlConnection(self,spineJoints)
+        self.SpineJointGrp = TDSpineRigging.setSpineRigLayering(self,spineJoints[0])
         #TDSpineRigging.createSpineNull(self)
-        TDSpineRigging.setSpineCtlLayering(self)
         #TDSpineRigging.createSpineCtlNull(self)
-        self.rootConstraintGPList = TDSpineRigging.createRootCtlConstraint(self,parentName)
+        #self.rootConstraintGPList = TDSpineRigging.createRootCtlConstraint(self,parentName)
 
         print "--result spine setup--\n",
-        return self.SpineCtlList + [self.rootConstraintGPList]
+        return [self.SpineCtlList[0],self.SpineCtlList[1],self.SpineJointGrp]
+
+    #背骨リグ用の新しいジョイントを作成
+    def createSpineJoint(self,spineJoints):
+        self.spineJoint = self.TDMRS.createRiggingJoint(spineJoints,1,"Add")
+
+        return self.spineJoint
+
+    #FKの背骨のジョイントを作成
+    def createSpineFKJoint(self,spineJoints):
+        self.FKSpineJoint = self.TDMRS.createRiggingJoint(spineJoints,0,"FK")
+
+        return self.FKSpineJoint
 
     #FKの背骨のコントローラーの作成
-    def createFKSpineCtl(self,CtlColor):
-        for spines in self.TDMRS.getSpineJoint():
-            self.spineCtl = self.TDMRS.createRigController(self.TDMRS.TDcrc.TDCircle,spines,spines,CtlColor,20)
-            self.spineCtls.append(self.spineCtl)
+    def createSpineFKCtl(self,spineJoints,CtlColor):
+        for spines in range(len(spineJoints)):
+            self.spineCtl = self.TDMRS.createRigController(self.TDMRS.TDcrc.TDCircle,spineJoints[spines],spineJoints[spines],CtlColor,20)
+            if spines > 0:
+                cmds.parent(self.spineCtl[0],self.spineCtls[spines-1])
+            self.spineCtls.append(self.spineCtl[1])
+            self.spineCtlOffset.append(self.spineCtl[0])
 
-        self.spineCtlGP = self.TDMRS.createGP(self.spineCtls[0],"%s_GP"%self.spineCtls[0])
+        self.spineCtlGP = self.TDMRS.createGP(self.spineCtlOffset[0],"%s_Grp"%self.spineCtls[0])
         return [self.spineCtlGP, self.spineCtls[-1]]
 
     #FK背骨コントローラーの関連付け
-    def createFKSpineCtlConnection(self):
-        for spines in range(len(self.TDMRS.getSpineJoint())):
-            cmds.parentConstraint(self.spineCtls[spines],self.TDMRS.getSpineJoint()[spines],mo=True)
+    def createSpineFKCtlConnection(self,spineJoints):
+        for spines in range(len(spineJoints)):
+            #cmds.parentConstraint(self.FKSpineJoint[spines],self.spineJoint[spines],mo=True)
+            #cmds.parentConstraint(self.spineCtls[spines],self.FKSpineJoint[spines],mo=True)
+            self.TDMRS.matrixConstraint(self.FKSpineJoint[spines],self.spineJoint[spines])
+            self.TDMRS.matrixConstraint(self.spineCtls[spines],self.FKSpineJoint[spines])
 
+    #背骨リグの階層分け
+    def setSpineRigLayering(self,spineJoints):
+        self.spineJointGrp = cmds.group(self.spineJoint[0],self.FKSpineJoint[0],name="%s_Joint_Grp"%spineJoints)
+
+        return self.spineJointGrp
+    """
     #背骨ジョイントのオフセットノードを作成
     def createSpineNull(self):
         for spineNulls in self.getSpineJoint():
@@ -49,15 +76,9 @@ class TDSpineRigging():
             self.spineCtlNullGP = self.createGP(spineCtlNulls,"%s_Offset"%spineNulls)
             self.spineCtlNull.append(self.spineCtlNullGP)
 
-    #背骨のコントローラーの階層分け
-    def setSpineCtlLayering(self):
-        self.spineCtls.reverse()
-        for spineLayers in range(len(self.spineCtls)-1):
-            cmds.parent(self.spineCtls[spineLayers],self.spineCtls[spineLayers+1])
-        self.spineCtls.reverse()
-
     #ルートコントローラー関連づけ用のヌルを作成
     def createRootCtlConstraint(self,parentName):
         self.rootConstraintGP = self.TDMRS.createModuleParentNull(parentName,self.spineCtls[0])
 
         return self.rootConstraintGP
+    """
