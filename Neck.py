e4890591df8bfea1cc8bcc1bc5f20785af77d602
Ryuji Tada <ezwed-dewze-v3@ezweb.ne.jp>
2016-11-30 20:42:38 +0900

    Create Neck Rig

--- Neck.py
+++ Neck.py
@@ -3,27 +3,51 @@ import maya.cmds as cmds
 from TDModularRiggingSystem import RiggingSystem
 
 #頭のリギング
-class TDHeadRigging():
+class TDNeckRigging():
     def __init__(self):
         self.TDMRS  = RiggingSystem.ModularRiggingSystem()
+        self.neckCtls    = [] #首コントローラー
+        self.neckCtlOffset    = [] #首オフセットコントローラー
 
     "-----コマンドの実行-----"
-    def setHeadRigging(self,CtlName,parentName,CtlColor):
-        self.NeckCtlList = TDHeadRigging.createFKNeckCtl(self,CtlName,CtlColor)
-        TDHeadRigging.createHeadCtlConnection(self)
-        self.spineConstraintGPList = TDHeadRigging.createSpineCtlConstraint(self,parentName)
+    def setNeckRigging(self,neckJoints,CtlColor):#,CtlName,parentName
+        TDNeckRigging.createNeckJoint(self,neckJoints)
+        self.NeckCtlList = TDNeckRigging.createNeckFKCtl(self,neckJoints,CtlColor)
+        """
+        TDNeckRigging.createHeadCtlConnection(self)
+        self.spineConstraintGPList = TDNeckRigging.createSpineCtlConstraint(self,parentName)
 
         print "--result Neck setup--\n",
         return self.NeckCtlList + [self.spineConstraintGPList]
+        """
+
+    #首リグ用の新しいジョイントを作成
+    def createNeckJoint(self,neckJoints):
+        self.neckJoint = self.TDMRS.createRiggingJoint(neckJoints,1,"Add")
+
+        return self.neckJoint
+
+    #FKの背骨のコントローラーの作成
+    def createNeckFKCtl(self,neckJoints,CtlColor):
+        for necks in range(len(neckJoints)):
+            self.neckCtl = self.TDMRS.createMatrixRigController(self.TDMRS.TDcrc.TDCircle,neckJoints[necks],neckJoints[necks],CtlColor,15)
+            if necks > 0:
+                cmds.parent(self.neckCtl[0],self.neckCtls[necks-1])
+            self.neckCtls.append(self.neckCtl[1])
+            self.neckCtlOffset.append(self.neckCtl[0])
+
+        self.neckCtlGP = self.TDMRS.createGP(self.neckCtlOffset[0],"%s_Grp"%self.neckCtls[0])
+        return [self.neckCtlGP, self.neckCtls[-1]]
+
 
     #FKの首のコントローラー
-    def createFKNeckCtl(self,CtlName,CtlColor):
-        self.neckCtl = self.TDMRS.createMatrixRigController(self.TDMRS.TDcrc.TDCircle,CtlName,CtlName,CtlColor,15)
-        self.neckJointGP = self.TDMRS.createChildAttrToParentGP(CtlName,"%sConnection_From__%s"%(CtlName,self.neckCtl))
-        self.neckCtlGP = self.TDMRS.createGP(self.neckCtl,"%s_GP"%self.neckCtl)
-        self.neckCtlOffset = self.TDMRS.createChildAttrToParentGP(self.neckCtl,"%s_Offset"%self.neckCtl)
+    def createFKNeckCtl(self,neckJoints,CtlColor):
+        self.neckCtl = self.TDMRS.createMatrixRigController(self.TDMRS.TDcrc.TDCircle,neckJoints,neckJoints,CtlColor,15)
+        #self.neckJointGP = self.TDMRS.createChildAttrToParentGP(neckJoints,"%sConnection_From__%s"%(neckJoints,self.neckCtl))
+        #self.neckCtlGP = self.TDMRS.createGP(self.neckCtl,"%s_GP"%self.neckCtl)
+        #self.neckCtlOffset = self.TDMRS.createChildAttrToParentGP(self.neckCtl,"%s_Offset"%self.neckCtl)
 
-        return [self.neckCtlGP, self.neckCtl]
+        #return [self.neckCtlGP, self.neckCtl]
 
     #FKの頭のコントローラーの関連づけ
     def createHeadCtlConnection(self,*argv):
