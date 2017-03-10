78fb2bc5494e7165054e4bb0687e0cfaab0579e6
Ryuji Tada <ezwed-dewze-v3@ezweb.ne.jp>
2017-03-10 22:50:14 +0900

    170310

--- modules/Spine.py
+++ modules/Spine.py
@@ -75,19 +75,22 @@ class TDSpineRigging():
             self.spineCtls.append(self.spineCtl[1])
             self.spineCtlOffset.append(self.spineCtl[0])
 
-        self.spineCtlGP = self.System.createGP(self.spineCtlOffset[0],"%s_Grp"%self.spineCtls[0])
+        self.spineCtlGP = self.System.createCenterPivotGP(self.spineCtlOffset[0],"%s_Grp"%self.spineCtls[0])
         return [self.spineCtlGP, self.spineCtls[-1]]
 
     #IKの背骨のコントローラーの作成
     def createSpineIKCtl(self,spineJoints,CtlColor,CtlScale):
         for spines in range(len(self.spineIKLoc)):
             self.spineIKCtl = self.System.createRigController(self.System.TDcrc.TDCube,"IK_"+spineJoints[spines],self.spineIKLoc[spines],CtlColor,20*CtlScale)
-            if spines > 0:
-                cmds.parent(self.spineIKCtl[0],self.spineIKCtls[spines-1])
+            cmds.setAttr(self.spineIKCtl[0]+".scale",2,0.3,2)
+            cmds.makeIdentity(self.spineIKCtl[0],apply=True,s=True)
+            #if spines > 0:
+                #cmds.parent(self.spineIKCtl[0],self.spineIKCtls[spines-1])
             self.spineIKCtls.append(self.spineIKCtl[1])
             self.spineIKCtlOffset.append(self.spineIKCtl[0])
+            cmds.parent(self.spineIKLoc[spines],self.spineIKCtl[1])
 
-        self.spineIKCtlGP = self.System.createGP(self.spineIKCtlOffset[0],"%s_Grp"%self.spineIKCtls[0])
+        self.spineIKCtlGP = self.System.createCenterPivotGP(self.spineIKCtlOffset,"%s_Grp"%self.spineIKCtls[0])
         return [self.spineIKCtlGP, self.spineIKCtls[-1]]
 
     #FK背骨コントローラーの関連付け
@@ -106,24 +109,22 @@ class TDSpineRigging():
     def createSpineIKCtlConnection(self,spineJoints):
         self.IKBlendCList = []
         for spines in range(len(spineJoints)):
-            if spines == 0:
-                pass
-            else:
-                self.IKBlendC = cmds.parentConstraint(self.IKSpineJoint[spines],self.spineJoint[spines],mo=True)
-                #cmds.parentConstraint(self.spineCtls[spines],self.IKSpineJoint[spines],mo=True)
-                self.IKBlendCList.append(self.IKBlendC[0]+"."+self.IKSpineJoint[spines]+"W1")
+            self.IKBlendC = cmds.parentConstraint(self.IKSpineJoint[spines+1],self.spineJoint[spines],mo=True)
+            #cmds.parentConstraint(self.spineCtls[spines],self.IKSpineJoint[spines],mo=True)
+            self.IKBlendCList.append(self.IKBlendC[0]+"."+self.IKSpineJoint[spines+1]+"W1")
+        cmds.orientConstraint(self.spineIKLoc[-1],self.IKSpineJoint[-1],mo=True)
 
         return self.IKBlendCList
 
     #IKFKスイッチコントローラーを作成
     def createSpineFKIKSwitchCtl(self,spineJoints,CtlScale):
-        self.SpineFKIKSwitchCtl = self.System.createRigController(self.System.TDcrc.TDCross,"%sFKIKSwitch"%spineJoints[0],spineJoints[-1],20,1*CtlScale)
+        self.SpineFKIKSwitchCtl = self.System.createRigController(self.System.TDcrc.TDCross,"%sFKIKSwitch"%spineJoints[0],spineJoints[1],20,1*CtlScale)
         self.getPosition1 = cmds.xform(self.IKSpineJoint[0],q=True,ws=True,rp=True)
         self.getPosition2 = cmds.xform(self.IKSpineJoint[-1],q=True,ws=True,rp=True)
         self.getPosition = [self.getPosition1[0]-self.getPosition2[0],self.getPosition1[1]-self.getPosition2[1],self.getPosition1[2]-self.getPosition2[2]]
         cmds.setAttr(self.SpineFKIKSwitchCtl[0]+".rotateX",90)
         cmds.setAttr(self.SpineFKIKSwitchCtl[0]+".translateX",-self.getPosition[1]*1)
-        cmds.pointConstraint(self.spineJoint[-1],self.SpineFKIKSwitchCtl[1],mo=True)
+        cmds.pointConstraint(self.spineJoint[1],self.SpineFKIKSwitchCtl[1],mo=True)
 
         self.System.createHideAttr(self.SpineFKIKSwitchCtl[1],"t",1,0)
         self.System.createHideAttr(self.SpineFKIKSwitchCtl[1],"r",1,0)
@@ -155,17 +156,16 @@ class TDSpineRigging():
     def createSpineFKIKSwitchConnection(self,spineJoints):
         #コントローラーのVisibility
         self.reverse = cmds.shadingNode("reverse",asUtility=True,n="%sFKIKReverse"%spineJoints[-1])
-        """
-        self.IKcondition = cmds.shadingNode("condition",asUtility=True,n="%sIKcondition1"%footJoints[-1])
-        self.FKcondition = cmds.shadingNode("condition",asUtility=True,n="%sFKcondition2"%footJoints[-1])
-        cmds.connectAttr(self.FootSwitch,self.IKcondition+".firstTerm")
-        cmds.connectAttr(self.FootSwitch,self.FKcondition+".firstTerm")
+        self.IKcondition = cmds.shadingNode("condition",asUtility=True,n="%sIKcondition1"%spineJoints[-1])
+        self.FKcondition = cmds.shadingNode("condition",asUtility=True,n="%sFKcondition2"%spineJoints[-1])
+        cmds.connectAttr(self.SpineSwitch,self.IKcondition+".firstTerm")
+        cmds.connectAttr(self.SpineSwitch,self.FKcondition+".firstTerm")
         cmds.setAttr(self.FKcondition+".secondTerm",1)
-        cmds.connectAttr(self.FKcondition+".outColorR", self.FKFootCtlGP+".visibility")
-        cmds.connectAttr(self.IKcondition+".outColorR", self.footIKCtlGP+".visibility")
-        cmds.connectAttr(self.IKcondition+".outColorR", self.footIKGP+".visibility")
-        cmds.connectAttr(self.IKcondition+".outColorR", self.annotateGrp+".visibility")
-        """
+        cmds.connectAttr(self.FKcondition+".outColorR", self.spineCtlGP[0]+".visibility")
+        cmds.connectAttr(self.IKcondition+".outColorR", self.spineIKCtlGP[0]+".visibility")
+        #cmds.connectAttr(self.IKcondition+".outColorR", self.footIKGP+".visibility")
+        #cmds.connectAttr(self.IKcondition+".outColorR", self.annotateGrp+".visibility")
+
         #コンストレイントのウエイト値
         cmds.connectAttr(self.SpineSwitch, self.reverse+".inputX")
         for FKBlendCLists in self.FKBlendCList:
@@ -175,15 +175,14 @@ class TDSpineRigging():
 
     #親となるモジュールとの関連付け用のヌルを作成
     def createSpineRigConnectParentNull(self,parentJoints):
-        self.ConnectionGrp = self.System.createModuleConnectionNull(self.spineCtlGP, parentJoints)
+        self.ConnectionGrp = self.System.createModuleConnectionNull([self.spineCtlGP[0],self.spineIKCtlGP[0]], parentJoints)
 
         return self.ConnectionGrp
 
     #背骨リグの階層分け
     def createSpineRigConnectNull(self,spineJoints):
         #self.ConnectionGrp = self.System.createModuleConnectionNull(self.spineCtlGP, spineJoints)
-        self.spineCtlGrp = cmds.group(self.ConnectionGrp,name="%s_Ctl_Grp"%spineJoints[0])
-        self.spineJointGrp = cmds.group(self.spineJoint[0],self.FKSpineJoint[0],self.IKSpineJoint[0],name="%s_Joint_Grp"%spineJoints[0])
-        cmds.xform([self.spineCtlGrp,self.spineJointGrp],piv=[0,0,0])
+        self.spineCtlGrp = self.System.createCenterPivotGP(self.ConnectionGrp,name="%s_Ctl_Grp"%spineJoints[0])
+        self.spineJointGrp = self.System.createCenterPivotGP([self.spineJoint[0],self.FKSpineJoint[0],self.IKSpineJoint[0]],name="%s_Joint_Grp"%spineJoints[0])
 
         return self.spineCtlGrp,self.spineJointGrp
